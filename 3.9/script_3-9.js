5c1f75dbe5e984f31c33eef1fe943e3c04014f25
Fripono.dev <fripono.dev@gmail.com>
2018-10-01 13:02:15 +0500

    3-9

---
+++ 3.9/script_3-9.js
@@ -0,0 +1,8 @@
+﻿console.log("Lesson 3.9");
+
+document.body.style.backgroundColor=prompt("Какой будет фон у страницы?");
+document.querySelector(".page").style.color=prompt("Какой будет цвет текста на странице?");
+document.getElementsByClassName("name")[0].innerHTML=prompt("Как зовут человека, который вас вдохновляет?");
+document.getElementsByTagName("img")[0].setAttribute("src",prompt("Введите адрес картинки:"));
+document.querySelector(".shortBio").innerHTML+=" "+prompt("Введите текст страницы:");
+document.querySelector(".shortBio").className+=" animated";
