9e7b58824e7a03c40203f0d0b2c1dc96689f060b
Fripono.dev <fripono.dev@gmail.com>
2018-10-01 23:04:00 +0500

    4.2

---
+++ 4.2/script.js
@@ -0,0 +1,22 @@
+﻿const rock = 0;
+const scissors = 1;
+const paper = 2;
+
+// попросим игрока ввести ответ в виде числа
+let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
+// Рандомное число 0, 1 или 2 — разберем это позже, пока не заморачивайтесь
+let computer = Math.floor(Math.random() * 3);
+
+// опишем все условия и будем выводить в консоль 'computer win' или 'player win'
+
+console.log("0 - rock, 1 - scissors, 2 - paper");
+
+if ((player==0 && computer==0) || (player==1 && computer==1) || (player==2 && computer==2)){
+	console.log("Player: "+player+"; Computer: "+computer+". DRAW!");
+}else if ((player==0 && computer==1) || (player==1 && computer==2) || (player==2 && computer==0)){
+	console.log("Player: "+player+"; Computer: "+computer+". PLAYER WIN!");
+}else if ((player==0 && computer==2) || (player==1 && computer==0) || (player==2 && computer==1)){
+	console.log("Player: "+player+"; Computer: "+computer+". COMPUTER WIN!");
+}else{
+	location.reload();
+}
