4ae377d85f172ea32cf68473efa64418ec3216fb
Fripono.dev <fripono.dev@gmail.com>
2018-10-10 01:50:10 +0500

    5.1

---
+++ 5.1/script.js
@@ -0,0 +1,17 @@
+console.log(document.getElementsByTagName("title")[0]);
+
+function moduleSquare(num){
+	return Math.abs(num)/2;
+}
+
+console.log(moduleSquare(-3));
+
+console.log(Math.floor(2.333));
+
+console.log(Math.round(2.7345));
+
+console.log(Math.random());
+
+console.log(Math.random()*(10-5)+5);
+
+console.log(Math.floor(Math.random()*(10-5)+5));
---
+++ 5.1/script_5.1.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByTagName("title")[0]);
+
+function lesson_5_1(num){
+	return Math.abs(num**Math.floor(Math.random()*(10-1)+1));
+}
+
+console.log(lesson_5_1(2));
