411a9cb898a0b83a9f80205d4f5d2e98db391c64
Fripono.dev <fripono.dev@gmail.com>
2018-10-25 23:02:53 +0500

    6.6

--- 6.6/script_6.6.js
+++ 6.6/script_6.6.js
@@ -3,16 +3,16 @@ class Custumer{
         this.name=name;
     }
     buy(){
-        return "Куплено";
+        console.log("Куплено");
     }
 }
 
 class childCustumer extends Custumer{
     getPresent(){
-        return "Шарик в подарок";
+        console.log("Шарик в подарок");
     }
 }
 
 const Piter=new childCustumer("Петя");
 
-console.log(Piter.name+", "+Piter.buy()+"! "+Piter.getPresent());
+Piter.buy();
